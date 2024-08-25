df5200a6065ef0df2f2778e672f9f577f4b6edfc
Mohamed Belhedi <mohamed.belhedi@yadbo.com>
2024-08-25 13:41:00 +0200

    Update v 1.0 with opencv file deletetd

--- streamlitapp.py
+++ streamlitapp.py
@@ -122,8 +122,6 @@ def SquatCounter():
         os.remove('stop_signal.txt')
 
 
-if __name__ == "__main__":
-    SquatCounter()
 st.set_page_config()
 st.title("Squatcounter")
 st.button(on_click=SquatCounter(), label='start cam')
